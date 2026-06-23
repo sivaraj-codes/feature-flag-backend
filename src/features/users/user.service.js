649e86b418ac2c7e635817973bcc43a9d4f97077
Sivaraj M <sivaraj2siva@gmail.com>
2026-06-23 17:47:41 +0530

    fix: user reg with invalid org validation

--- src/features/users/user.service.js
+++ src/features/users/user.service.js
@@ -1,6 +1,7 @@
 import { HTTP_STATUS, MESSAGES } from "../../constants/responseConstants.js";
 import { ALLOWED_SIGNUP_ROLES } from "../../constants/roleConstants.js";
 import { AppError } from "../../shared/errors/AppError.js";
+import { Organization } from "../organizations/org.model.js";
 import { User } from "./user.model.js";
 import * as userRepository from "./user.repository.js";
 import bcrypt from "bcryptjs";
@@ -17,6 +18,9 @@ export const createUser = async ({
   if (!ALLOWED_SIGNUP_ROLES.includes(roleId)) {
     throw new AppError("Invalid role for signup", HTTP_STATUS.BAD_REQUEST);
   }
+
+  const org = await Organization.findById(organizationId);
+  if (!org) throw new AppError("Organization not found");
 
   const passwordHash = await bcrypt.hash(password, 10);
 

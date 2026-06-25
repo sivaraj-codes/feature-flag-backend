8b3f19138714778723f644dee0ac05a27aee8922
Sivaraj M <sivaraj2siva@gmail.com>
2026-06-25 17:06:44 +0530

    fix: cors issue

--- src/shared/utils/cookies.js
+++ src/shared/utils/cookies.js
@@ -2,7 +2,7 @@ export const setAuthCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true, // not accessible via JS
     secure: process.env.NODE_ENV === "production", // HTTPS only in prod
-    sameSite: "strict", // CSRF protection
+    sameSite: "none", // For cross-origin SPA + API deployments:
     maxAge: 8 * 60 * 60 * 1000, // 8 hours in ms
   });
 };

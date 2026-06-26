8f341ad6cb1ff5393008cbdd47d71d82b8fff382
Sivaraj M <sivaraj2siva@gmail.com>
2026-06-26 10:40:22 +0530

    fix: updated expiredToken Error

--- src/shared/middlewares/authenticate.js
+++ src/shared/middlewares/authenticate.js
@@ -9,7 +9,17 @@ export const authenticate = (req, res, next) => {
     throw new AppError("No token provided", HTTP_STATUS.UNAUTHORIZED);
   }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = decoded;
-  next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      throw new AppError(
+        "Session expired, please login again",
+        HTTP_STATUS.UNAUTHORIZED,
+      );
+    }
+    throw new AppError("Invalid token", HTTP_STATUS.UNAUTHORIZED);
+  }
 };

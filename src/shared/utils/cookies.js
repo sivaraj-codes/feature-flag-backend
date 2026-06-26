6b48485f1c35b6ae39d5ac074857882069772ee0
Sivaraj M <sivaraj2siva@gmail.com>
2026-06-26 12:07:19 +0530

    fix: cors issue in development

--- src/shared/utils/cookies.js
+++ src/shared/utils/cookies.js
@@ -1,8 +1,9 @@
 export const setAuthCookie = (res, token) => {
+  const isProd = process.env.NODE_ENV === "production";
   res.cookie("token", token, {
     httpOnly: true, // not accessible via JS
-    secure: process.env.NODE_ENV === "production", // HTTPS only in prod
-    sameSite: "none", // For cross-origin SPA + API deployments:
+    secure: isProd, // HTTPS only in prod
+    sameSite: isProd ? "none" : "lax", // none-For cross-origin SPA + API deployments:  lax- works for local same-machine dev
     maxAge: 8 * 60 * 60 * 1000, // 8 hours in ms
   });
 };
